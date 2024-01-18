f47b7f8c40e985eb30ca794af869895e3c9ab86f
Sammy Tang <webdeveloper@warmoth.com>
2024-01-18 15:59:25 -0800

    Started work on the portfolio

---
+++ src/data.js
@@ -0,0 +1,49 @@
+export const projects = [
+  {
+    title: "Blobber Royale",
+    subtitle: "Node.js Socket.io",
+    description:
+      "A voice controlled game that recognizes language, with the goal of knocking the opponent out of the arena.",
+    image: "./project-1.gif",
+    link: "https://github.com/yomyyo/yelling-game",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const skills = [
+  "Java",
+  "Python",
+  "Node",
+  "JavaScript",
+  "React",
+  "CSS",
+  "Tailwind",
+  "Bootstrap",
+  "MySql",
+  "AWS",
+  "Docker",
+  "Jenkins"
+];
